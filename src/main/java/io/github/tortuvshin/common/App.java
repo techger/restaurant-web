fa99ccc0daed83346b102b527cf45c2dc4caf511
tortuvshin <toroo.byamba@gmail.com>
2016-04-03 22:11:49 +0900

    update

--- src/main/java/io/github/tortuvshin/common/App.java
+++ src/main/java/io/github/tortuvshin/common/App.java
@@ -1,5 +1,7 @@
 package io.github.tortuvshin.common;
 
+import org.hibernate.InvalidMappingException;
+import org.springframework.beans.factory.BeanCreationException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -8,12 +10,24 @@ import io.github.tortuvshin.natoriat.bo.NatoriatBo;
 import io.github.tortuvshin.natoriat.model.Natoriat;
 
 public class App {
-
+	static ApplicationContext appContext;
 	public static void main(String[] args) {
-
-		ApplicationContext appContext = 
-				new ClassPathXmlApplicationContext("spring/config/BeanLocations.xml");
-		System.out.println(appContext);
+		try {
+			appContext = 
+					new ClassPathXmlApplicationContext("spring/config/BeanLocations.xml");
+		}
+		catch (BeanCreationException be){
+			System.out.println("Алдаа BeanCreationException: "+be);
+		}
+		catch (InvalidMappingException ie){
+			System.out.println("Алдаа InvalidMappingException: "+ie);			
+		} 
+		catch (NullPointerException ex) {
+			System.out.println("Алдаа NullPointerException: "+ex);
+		} 
+		catch (Exception e){
+			System.out.println("Алдаа: "+e);
+		}
 		NatoriatBo natoriatBo = (NatoriatBo)appContext.getBean("natoriatBo");
 		
 		Natoriat natoriat = new Natoriat();
